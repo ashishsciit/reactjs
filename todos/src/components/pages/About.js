d470825c337e242da7a854fd3461eb74cc07bbef
Ashish Kumar Shyamlal <ashishsciit@gmail.com>
2020-12-26 11:26:46 +0530

    Completed reactjs todo app with jsonplaceholder API

---
+++ todos/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About Us</h1>
+            <p>This is todo list app v1.0.1 Its part of reactjs learning.</p>
+        </React.Fragment>
+    );
+}
+
+export default About;
